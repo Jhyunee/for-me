e07ba77c0a09e82357ab63963f45cedf5515be2c
Song Jiwon <zzionsaving@naver.com>
2024-04-05 13:16:34 +0900

    todo

---
+++ todo-app/App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Header from './Header'
+import Body from './Body'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: "#EEE",
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 30, 
+    marginLeft: 20,
+    marginBottom: 20,
+  }
+});
+
+export default class App extends React.Component {
+
+state = {
+  todos: []
+}
+
+addTodo = (todo) => {
+  const newTodo = {
+    id: Date.now(),
+    text: todo,
+    completed: false,
+  } 
+  this.setState(prevState => {
+    const todos = [
+      newTodo,
+      ...prevState.todos
+    ];
+    AsyncStorage.setItem("todos", JSON.stringify(todos));
+    return { todos }
+  });
+}
+checkTodo = (id) => {
+  this.setState(prevState => {
+    const [ todo ] = prevState.todos.filter(e => e.id === id);
+    todo.completed = !todo.completed;
+    const todos = [
+      ...prevState.todos
+    ];
+    AsyncStorage.setItem("todos", JSON.stringify(todos));
+    return ({ todos })
+  });
+}
+removeTodo = (id) => {
+  this.setState(prevState => {
+    const index = prevState.todos.findIndex(e => e.id === id);
+    prevState.todos.splice(index, 1);
+    const todos = [
+      ...prevState.todos
+    ];
+    AsyncStorage.setItem("todos", JSON.stringify(todos));
+    return ({ todos })
+  });
+}
+
+  componentDidMount = () => {
+    AsyncStorage.getItem("todos").then(data => {
+      const todos = JSON.parse(data || '[]');
+      this.setState({todos});
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Todo App</Text>
+        <Header addTodo={this.addTodo}/>
+        <Body todos={this.state.todos} checkTodo={this.checkTodo} removeTodo={this.removeTodo}/>
+      </View>
+    );
+  }
+}
